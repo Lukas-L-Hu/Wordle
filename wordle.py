247de1d2fa33020b0c8c09ca061f1cd7393c18ef
Lukas Hu <lukashu22@gmail.com>
2025-05-11 20:13:24 -0700

    Defined new, basic functions

--- wordle.py
+++ wordle.py
@@ -3,8 +3,16 @@ import math
 
 words = set()
 
+chosen_word = None
 
+def generate_chosen_word():
+    pass
 
+def is_valid_word():
+    pass
+
+def check_correct_letters():
+    pass
 
 if __name__ == "__main__":
     pass
